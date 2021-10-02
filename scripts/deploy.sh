b434e3147a01092f1cca44e0c2c737134b3cbffc
Gyunny <wjdrbs966@naver.com>
2021-10-03 00:06:18 +0900

    test: jar local.yml 받도록 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,4 +21,4 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/mash/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/mash/deploy_err.log &
+nohup java -jar -Dspring.profiles.active=local $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/mash/deploy_err.log &
