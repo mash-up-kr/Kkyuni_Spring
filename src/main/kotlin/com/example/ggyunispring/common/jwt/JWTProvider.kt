638e79f1a07112d0ecb1b99540a63494f42acf5c
Gyunny <wjdrbs966@naver.com>
2021-10-26 00:02:51 +0900

    Refactor: JWT Secret Key Value 주입 변경

--- src/main/kotlin/com/example/ggyunispring/common/jwt/JWTProvider.kt
+++ src/main/kotlin/com/example/ggyunispring/common/jwt/JWTProvider.kt
@@ -1,39 +1,57 @@
 package com.example.ggyunispring.common.jwt
 
-import com.example.ggyunispring.common.enum.JWT
 import com.example.ggyunispring.common.enum.JWT.KEY_VALUE_EMPTY
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jws
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
 import java.util.*
+import javax.annotation.PostConstruct
 import javax.servlet.http.HttpServletRequest
 
 @Component
 class JwtProvider {
-    private val secretKey: String = Base64.getEncoder().encodeToString("secretKey".encodeToByteArray())
+
+    @Value("\${jwt.secret}")
+    private lateinit var secretKey: String
+
     private val tokenValidTime: Long = 30 * 24 * 60 * 60 * 1000L
     private val refreshTokenValidTime: Long = 365 * 24 * 60 * 60 * 1000L
+
     companion object {
         val HEADER_NAME: String = "Authorization"
+    }
+
+    @PostConstruct
+    fun init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.encodeToByteArray())
     }
 
     fun createToken(sub: String): String {
         val claims = Jwts.claims().setSubject(sub)
         val date = Date()
-        return Jwts.builder().setClaims(claims).setIssuedAt(date).setExpiration(Date(date.time + tokenValidTime))
-            .signWith(SignatureAlgorithm.HS256, secretKey).compact()
+        return Jwts.builder()
+            .setClaims(claims)
+            .setIssuedAt(date)
+            .setExpiration(Date(date.time + tokenValidTime))
+            .signWith(SignatureAlgorithm.HS256, secretKey)
+            .compact()
     }
 
     fun createRefreshToken(sub: String): String {
         val claims = Jwts.claims().setSubject(sub)
         val date = Date()
-        return Jwts.builder().setClaims(claims).setIssuedAt(date).setExpiration(Date(date.time + refreshTokenValidTime))
-            .signWith(SignatureAlgorithm.HS256, secretKey).compact()
+        return Jwts.builder()
+            .setClaims(claims)
+            .setIssuedAt(date)
+            .setExpiration(Date(date.time + refreshTokenValidTime))
+            .signWith(SignatureAlgorithm.HS256, secretKey)
+            .compact()
     }
 
     fun getAuthentication(token: String, userDetailsService: UserDetailsService): Authentication {
