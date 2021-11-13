22103fe4a261ae05dc344ee3d4d05ec3eec631bf
Gyunny <wjdrbs966@naver.com>
2021-11-13 21:23:41 +0900

    Chore: 사용되지 않는 코드 제거

--- src/main/kotlin/com/example/ggyunispring/domain/repository/LogoRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ggyunispring.domain.repository
-
-import com.example.ggyunispring.domain.entity.Logo
-import org.springframework.data.repository.CrudRepository
-
-interface LogoRepository: CrudRepository<Logo, Long> {
-}
