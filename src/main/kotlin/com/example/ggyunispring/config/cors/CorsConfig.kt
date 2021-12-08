033bb0ec39427a7aa542947b2cc099b5ccbf3569
Gyunny <wjdrbs966@naver.com>
2021-12-08 18:37:57 +0900

    Fix: Swagger Error By @EnableWebMvc 어노테이션 삭제

--- src/main/kotlin/com/example/ggyunispring/config/cors/CorsConfig.kt
+++ src/main/kotlin/com/example/ggyunispring/config/cors/CorsConfig.kt
@@ -2,11 +2,9 @@ package com.example.ggyunispring.config.cors
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@EnableWebMvc
 class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/v1/**")
