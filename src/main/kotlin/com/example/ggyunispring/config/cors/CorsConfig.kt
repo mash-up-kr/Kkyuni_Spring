b65a9da709df566189f668d184eff9aadd081d55
Gyunny <wjdrbs966@naver.com>
2021-12-08 22:19:44 +0900

    Fix: CORS 해결

--- src/main/kotlin/com/example/ggyunispring/config/cors/CorsConfig.kt
+++ src/main/kotlin/com/example/ggyunispring/config/cors/CorsConfig.kt
@@ -9,7 +9,7 @@ class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/v1/**")
 //            .allowedOrigins("*")
-            .allowedOriginPatterns("netlify.app")
+            .allowedOriginPatterns("https://compassionate-wing-0abef6.netlify.app/")
             .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
             .allowedHeaders("Authorization")
             .allowCredentials(true).maxAge(3600)
