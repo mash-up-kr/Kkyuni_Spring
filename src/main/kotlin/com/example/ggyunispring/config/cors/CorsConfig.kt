be39d0f5fa895f6394d2d7f0b233b59d197fcf3e
Gyunny <wjdrbs966@naver.com>
2021-12-11 03:29:51 +0900

    Fix: CORS Error Test

--- src/main/kotlin/com/example/ggyunispring/config/cors/CorsConfig.kt
+++ src/main/kotlin/com/example/ggyunispring/config/cors/CorsConfig.kt
@@ -8,8 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/v1/**")
-            //.allowedOriginPatterns("*")
-            .allowedOrigins("https://compassionate-wing-0abef6.netlify.app")
+            .allowedOriginPatterns("*")
             .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
             .allowedHeaders("Authorization")
             .allowCredentials(true).maxAge(3600)
